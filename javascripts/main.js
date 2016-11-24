47560dd49ef1fdf2eaec25a29134bad2eb1c141a
Krzysztof Kirsz <k.kirsz@gmail.com>
2016-11-24 21:41:15 +0100

    slimer navbar, fixed when scrolling, shadow for header

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
 
+var previousOffset;
+
+document.addEventListener("scroll", function() {
+var bar = $("#navigation");
+console.log(previousOffset);
+if ((window.pageYOffset > 100) && (previousOffset <= 100)) {
+  console.log("t");
+  bar.removeClass("");
+  bar.addClass("navbar-fixed-top");
+}
+  
+else if (window.pageYOffset <= 100 && previousOffset > 100) {
+  console.log("s");
+  bar.removeClass("navbar-fixed-top");
+  bar.addClass("");
+}
+previousOffset = window.pageYOffset;
+});
